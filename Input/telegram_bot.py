b76cef4f3ddcee34442d38e3acc1043d605f7298
root <root@nanoti.me>
2018-10-11 00:03:34 +0000

    removed redundant code and unused imports

--- Input/telegram_bot.py
+++ Input/telegram_bot.py
@@ -1,5 +1,4 @@
 from telegram.ext import Updater,MessageHandler, CommandHandler, Filters
-import _thread
 import logging                                                                                                                                                                                                                                
 
 class Telegram_bot:
@@ -7,11 +6,9 @@ class Telegram_bot:
 		self.updater.start_polling()
 
 	def handler(self, bot, update):
-		#bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text[::-1])
 		self.callback(__name__, update)
 	
 	def setup(self):
-		#self.callback = callback_
 		logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 		self.updater = Updater(token='593203892:AAEtktfQUr89Mt1q1J_WbwikRHTdWpDqQDo')
 		dispatcher = self.updater.dispatcher
@@ -23,7 +20,6 @@ class Telegram_bot:
 		dispatcher.add_handler(commandHandler)
 
 		self.updater.start_polling()
-		#_thread.start_new_thread(self.start, (updater,))
 
 	def cleanup(self):
 		self.updater.stop()
